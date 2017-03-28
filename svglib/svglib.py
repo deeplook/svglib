deeplook/svglib

commit 2d4a7e25f0637f48d1d8b530a29e3889532f89d3

Don't warn when handling px units
No unit should be equivalent to px units.

svglib/svglib.py
@@ -225,7 +225,7 @@ class Svg2RlgAttributeConverter(AttributeConverter):
         elif text.endswith("pt"):
             return float(text[:-2]) * 1.25
 
-        for unit in ("em", "ex", "px"):
+        for unit in ("em", "ex"):
             if unit in text:
                 logger.warn("Ignoring unit: %s" % unit)
                 text = text.replace(unit, '')

commit 114a9f17439a234e1b51daa7cc8f2744d31787ab

Handle px units
Fixes a regression in 2d4a7e25f0637f48d1d8b530a29e3889532f89d3.

svglib/svglib.py
@@ -224,6 +224,8 @@ class Svg2RlgAttributeConverter(AttributeConverter):
             return float(text[:-2]) * pica
         elif text.endswith("pt"):
             return float(text[:-2]) * 1.25
+        elif text.endswith("px"):
+            return float(text[:-2])
 
         for unit in ("em", "ex"):
             if unit in text:

tests/test_basic.py
@@ -204,6 +204,7 @@ class TestLengthAttrConverter(object):
             ("1e1in", 10*inch),
             ("1e1%", 10),
             ("-8e-2cm", (-8e-2)*cm),
+            ("20px", 20),
             ("20pt", 20 * 1.25),
         )
         ac = svglib.Svg2RlgAttributeConverter()