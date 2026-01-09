78a593babc66b94bd0c8528670778f3ff2b2de7e
Nico Mexis <nico.mexis@kabelmail.de>
2026-01-09 17:27:27 +0100

    Update svg2rlg to accept IO as path type

--- src/svglib/svglib.py
+++ src/svglib/svglib.py
@@ -30,7 +30,7 @@ import shlex
 import shutil
 from collections import defaultdict, namedtuple
 from io import BytesIO
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, IO, List, Optional, Tuple, Union
 
 from PIL import Image as PILImage
 from reportlab.graphics.shapes import (
@@ -1782,7 +1782,7 @@ class Svg2RlgShapeConverter(SvgShapeConverter):
 
 
 def svg2rlg(
-    path: Union[str, os.PathLike[str]], resolve_entities: bool = False, **kwargs: Any
+    path: Union[str, os.PathLike[str], IO], resolve_entities: bool = False, **kwargs: Any
 ) -> Optional[Drawing]:
     """Convert an SVG file to a ReportLab Drawing object.
 
